d4569a43eed47525cca635626ec307574e0a2475
Sotiris Gkanouris <sotiris@messagebird.com>
2018-04-01 19:40:50 +0200

    Added more API actions

--- data/scripts/api_actions.py
+++ data/scripts/api_actions.py
@@ -1,44 +1,78 @@
 # -*- coding: utf-8 -*-
-import requests,os,json
+import requests
+import os
+import json
+mb_url = "https://voice.messagebird.com"
+mb_headers = {
+    "Authorization": "AccessKey " + os.environ['api_key'],
+    "X-MessageBird-Version": "20170314",
+    "Content-Type": "application/json; charset=utf-8",
+}
 
 
 def get_recording(call_id, leg_id, recording_id):
     try:
         response = requests.get(
-            url='https://voice.messagebird.com/calls/{callid}/legs/{legid}/recordings/{recordingid}'.format(callid=call_id, legid=leg_id, recordingid=recording_id)
-            headers={
-                "X-MessageBird-Version": "20170314",
-                "Authorization": "AccessKey " + os.environ['api_key'],
-                "Content-Type": "application/json",
-            })
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+            url=mb_url+"/calls/"+call_id+"/legs/"+leg_id+"/recordings/"+recording_id,
+            headers=mb_headers,
+        )
+        return response.content
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
-    
+        return None
+
+
+def get_recordings(call_id, leg_id):
+    try:
+        response = requests.get(
+            url=mb_url+"/calls/"+call_id+"/legs/"+leg_id+"/recordings/",
+            headers=mb_headers,
+        )
+        return response.content
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+        return None
+
+
+def get_leg(call_id, leg_id):
+    try:
+        response = requests.get(
+            url=mb_url+"/calls/"+call_id+"/legs/"+leg_id,
+            headers=mb_headers,
+        )
+        return response.content
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+        return None
+
+
+def get_call(call_id):
+
+    try:
+        response = requests.get(
+            url=mb_url + "/calls/"+call_id,
+            headers=mb_headers,
+        )
+        return response.content
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+        return None
+
 
 def create_webhook_request(token, server_url):
     try:
         response = requests.post(
-            url="https://voice.messagebird.com/webhooks",
-            headers={
-                "X-MessageBird-Version": "20170314",
-                "Authorization": "AccessKey " + os.environ['api_key'],
-                "Content-Type": "application/json",
-            },
+            url=mb_url+"/webhooks",
+            headers=mb_headers,
             data=json.dumps({
                 "url": ""+server_url+"",
                 "token": ""+token+""
             })
         )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+        return response.content
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
+        return None
 
 
 def create_callflow_for_number_request(number):
@@ -46,12 +80,8 @@ def create_callflow_for_number_request(number):
 
     try:
         response = requests.post(
-            url="https://voice.messagebird.com/numbers/"+number+"/call-flow",
-            headers={
-                "Content-Type": "application/json; charset=utf-8",
-                "Authorization": "AccessKey " + os.environ['api_key'],
-                "X-MessageBird-Version": "20170314",
-            },
+            url=mb_url+"/numbers/"+number+"/call-flow",
+            headers=mb_headers,
             data=json.dumps({
                 "title": "Test",
                 "steps": [
@@ -72,10 +102,7 @@ def create_callflow_for_number_request(number):
                 ]
             })
         )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+        return response.content
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
-
+        return None
